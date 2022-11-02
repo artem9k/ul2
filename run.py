30511388edc0b9fbe9b4b0b8a4fe6fe510aaa654
yatsenkoa <yatsenkoa2020@gmail.com>
2022-11-02 00:48:58 -0600

    helpers

--- run.py
+++ run.py
@@ -1,1 +1,8 @@
+from t5.test import Test
+from t5.t5 import T5ForConditionalGeneration
 print('hello, sanity check!')
+
+config = None
+T5ForConditionalGeneration(config)
+
+
---
+++ t5/test.py
@@ -0,0 +1,6 @@
+
+
+class Test():
+    def __init__(self):
+        self.test = 1
+        self.test2 = 2
